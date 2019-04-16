4ba9e346d72da256e926e092577989f966dec8ac
DangerN <cdneal091@gmail.com>
2019-04-16 10:11:01 -0600

    checkboxes how do they work

--- recipe-manger-project/app/models/recipe_ingredient.rb
+++ recipe-manger-project/app/models/recipe_ingredient.rb
@@ -1,4 +1,12 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :ingredient
   belongs_to :recipe
+
+  def ingredient
+    Ingredient.all.find{|ingredient| self.ingredient_id == ingredient.id}
+  end
+
+  def ingredient_name
+    ingredient.name
+  end
 end
