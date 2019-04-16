f27461348103ca9b4aee4f59cc056c70385c9ea5
DangerN <cdneal091@gmail.com>
2019-04-16 14:54:16 -0600

    more commits

--- recipe-manger-project/config/routes.rb
+++ recipe-manger-project/config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'recipe_ingredients/:id', to: 'recipes#ingredients_form', as: 'ingredients_form'
   post 'recipe_ingredients/:id', to: 'recipes#change_recipe_ingredients'
+  post 'add_user_recipe/:id', to: 'user#add_user_recipe', as: 'add_user_recipe'
 end
